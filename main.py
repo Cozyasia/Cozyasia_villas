184eed3534df54b67a991a169048cd0a0708f401
Cozyasia <cozy.asias@gmail.com>
2026-08-23 19:59:09 +0300

    Apply captured Premium emoji V6 layout

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ import post_standardizer
 import post_template_patch
 import post_throttle_patch
 import post_layout_v5
+import post_layout_v6_premium
 import emoji_calibration
 import template_capture_mode
 import channel_template_capture
@@ -27,6 +28,7 @@ catalog_feedback_patch.apply(cozy_catalog)
 post_template_patch.apply(post_standardizer)
 post_throttle_patch.apply(post_standardizer)
 post_layout_v5.apply(post_standardizer, post_throttle_patch)
+post_layout_v6_premium.apply(post_standardizer, post_throttle_patch)
 manual_edit_guard.apply(post_standardizer)
 
 log = logging.getLogger("villa-bot-wrapper")
